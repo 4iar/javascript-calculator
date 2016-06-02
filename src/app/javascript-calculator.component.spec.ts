6737f4f3e72f90dab04cefe27d50c2537447d767
4iar <4iar@users.noreply.github.com>
2016-06-02 23:49:35 +0100

    Add equals() function and tests

--- src/app/javascript-calculator.component.spec.ts
+++ src/app/javascript-calculator.component.spec.ts
@@ -19,4 +19,30 @@ describe('App: JavascriptCalculator', () => {
       inject([JavascriptCalculatorAppComponent], (app: JavascriptCalculatorAppComponent) => {
     expect(app.title).toEqual('javascript-calculator works!');
   }));
+
+  describe('Function: equals', () => {
+    it('it should return NaN if the expression is invalid',
+      inject([JavascriptCalculatorAppComponent], (app: JavascriptCalculatorAppComponent) => {
+        app.equation = '2++';
+        app.equals();
+        expect(app.display).toEqual('NaN');
+      }));
+
+    it('it should evaluate the expression in this.equation and put the result in this.display',
+      inject([JavascriptCalculatorAppComponent], (app: JavascriptCalculatorAppComponent) => {
+        app.equation = '2+2';
+        app.equals();
+        expect(app.display).toEqual('4');
+      }));
+
+    it('it should not convert numbers with leading zeroes to octal ',
+      inject([JavascriptCalculatorAppComponent], (app: JavascriptCalculatorAppComponent) => {
+        app.equation = '017';
+        app.equals();
+        expect(app.display).toEqual('17');
+      }));
+  });
+
+
+
 });
--- src/app/javascript-calculator.component.ts
+++ src/app/javascript-calculator.component.ts
@@ -8,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class JavascriptCalculatorAppComponent {
   title = 'javascript-calculator works!';
+
+  display = '';
+  equation = '';
+
+  isAnOperator (p) {
+    var operators = ['%', 'x', '-', '+'];
+    return (operators.indexOf(p) !== -1);
+  }
+
+  equals () {
+    var equationToEval = '';
+    var equationArray = this.equation.split(/(,|\+|%|-|x)/);
+
+    // Number() the number strings to prevent leading zero numbers from being converted to octal
+    // E.g. 023 === 19 by default
+    for (var i = 0; i < equationArray.length; i++) {
+      if (!this.isAnOperator(this.equation[i])) {
+        equationToEval += Number(equationArray[i]);
+      } else {
+        equationToEval += equationArray[i];
+      }
+    }
+
+    // Display NaN if the equation is invalid
+    // E.g. '2+++', '2+2+', etc...
+    var result = ''
+    try {
+      result = eval(equationToEval);
+    } catch (e) {
+      if (e instanceof ReferenceError || e instanceof SyntaxError) {
+        result = 'NaN'
+      }
+    } finally {
+      this.display = String(result);
+    }
+    this.equation = '';
+  }
 }
